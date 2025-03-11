658556f88bcdb92eb395f24ebce80a536e356112
unknown <2324290093@ACADEMICO.IESB.BR>
2025-03-10 21:58:22 -0300

    fazendo a aula 02 mais uma vez

--- aulas/aula02/screens/PadraoIOS.js
+++ aulas/aula02/screens/PadraoIOS.js
@@ -1,16 +1,33 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 function PadraoIOS() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "space-between",
-        backgroundColor: "seashell",
-      }}
-    >
-      <View style={{ height: 64, backgroundColor: "gray" }}></View>
-      <View style={{ height: 64, backgroundColor: "black"}}></View>
+    <View style={{ flex: 1, justifyContent: "space-between" }}>
+      <View
+        style={{
+          height: 64,
+          backgroundColor: "gray",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 24, fontWeight: "bold", color: "white" }}>
+          Meu App
+        </Text>
+      </View>
+      <View
+        style={{
+          height: 64,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          backgroundColor: "lightgray",
+        }}
+      >
+        <Text>Home</Text>
+        <Text>Perfil</Text>
+        <Text>Configurações</Text>
+      </View>
     </View>
   );
 }
