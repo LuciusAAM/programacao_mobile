f345feba4ef2f5ef6558b65399ae5fe964579a88
unknown <2324290093@academico.iesb.br>
2025-05-09 20:36:42 -0300

    continuando a aula07

--- aulas/aula07/App.js
+++ aulas/aula07/App.js
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import MainNavigator from "./routes/MainNavigator";
+import Login from "./screens/Login";
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <MainNavigator />
+        <Login />
       </NavigationContainer>
     </SafeAreaProvider>
   );
